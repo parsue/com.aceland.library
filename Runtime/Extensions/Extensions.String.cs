4c35e417657f36a1694fe2a36aa3675e7c053913
Parsue Choi <parsue@gmail.com>
2024-09-10 09:20:12 +0800

    Extension string add funciton
    SaveToFile
    LoadFromFile

--- Runtime/Extensions/Extensions.String.cs
+++ Runtime/Extensions/Extensions.String.cs
@@ -6,5 +6,23 @@ namespace AceLand.Library.Extensions
         {
             return string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value);
         }
+
+        public static async Task SaveToFile(this string data, string filename)
+        {
+            if (data.IsNullOrEmptyOrWhiteSpace()) return;
+
+            var filePath = Path.Combine(Application.persistentDataPath, filename);
+
+            if (!File.Exists(filePath))
+                await File.Create(filePath).DisposeAsync();
+
+            await File.WriteAllTextAsync(filePath, data);
+        }
+
+        public static async Task<string> LoadFromFile(this string filename)
+        {
+            var filePath = Path.Combine(Application.persistentDataPath, filename);
+            return !File.Exists(filePath) ? string.Empty : await File.ReadAllTextAsync(filePath);
+        }
     }
 }
