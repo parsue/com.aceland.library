50cc2a30b072805571bdab56dec28de857183741
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2024-11-01 15:12:40 +0800

    Build Leveling Utils

---
+++ Runtime/BuildLeveling/BuildLevel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AceLand.Library.BuildLeveling
+{
+    public enum BuildLevel
+    {
+        None,
+        Editor,
+        DevelopmentBuild,
+        Build,
+    }
+
+    public static class BuildLevelUtils
+    {
+        public static BuildLevel CurrentLevel
+        {
+            get
+            {
+#if UNITY_EDITOR
+                return BuildLevel.Editor;
+#elif DEBUG
+                return BuildLevel.DevelopmentBuild;
+#else
+                return BuildLevel.Build;
+#endif
+            }
+        }
+
+        public static bool IsAcceptedLevel(this BuildLevel level)
+        {
+#if UNITY_EDITOR
+            return level >= BuildLevel.Editor;
+#elif DEBUG
+            return level >= BuildLevel.DevelopmentBuild;
+#else
+            return level >= BuildLevel.Build;
+#endif
+        }
+    }
+}
