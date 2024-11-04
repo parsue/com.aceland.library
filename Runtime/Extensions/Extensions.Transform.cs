23f3d3934bddd6763041186a4c40078f7f5e9c06
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2024-11-04 10:01:23 +0800

    optimize Transform.Extension

--- Runtime/Extensions/Extensions.Transform.cs
+++ Runtime/Extensions/Extensions.Transform.cs
@@ -1,13 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AceLand.Library.Extensions
 {
     public static partial class Extensions
     {
-        public static void DeleteAllChildren(this Transform transform)
+        public static void DestroyAllChildren(this Transform transform)
         {
             while (transform.childCount > 0)
-                Object.Destroy(transform.GetChild(0).gameObject);
+            {
+                var child = transform.GetChild(0).gameObject;
+                if (Application.isPlaying) Object.DestroyImmediate(child);
+                else Object.Destroy(child);
+            }
+        }
+
+        public static void DestroyAllChildren<T>(this Transform transform) where T : MonoBehaviour
+        {
+            var list = new List<T>();
+            foreach (var child in transform)
+                if (child is T t) list.Add(t);
+
+            foreach (var item in list)
+            {
+                if (Application.isPlaying) Object.DestroyImmediate(item);
+                else Object.Destroy(item);
+            }
         }
 
         public static void CopyDataFrom(this Transform transform, Transform source, bool copyScale = false)
