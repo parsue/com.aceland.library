5b52c9d8e99229b8bad14a42ab92883e3dfd2457
Parsue Choi <parsue@gmail.com>
2024-11-06 16:27:39 +0800

    Create Singleton.cs

---
+++ Runtime/Mono/Singleton.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace AceLand.Mono
+{
+    public abstract class Singleton : MonoBehaviour
+    {
+        public virtual void SceneInit()
+        {
+            // noop
+        }
+    }
+    
+    public abstract class Singleton<T> : Singleton
+        where T : Singleton<T>
+    {
+        private static T _instance;
+
+        protected virtual void Awake()
+        {
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = GetComponent<T>();
+            transform.parent = null;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
