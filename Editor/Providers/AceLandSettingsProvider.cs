860bf00f8ef7f03268f029bd07cf4fea593934e9
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2024-11-21 13:34:09 +0800

    Handle Undo in Project Settings

---
+++ Editor/Providers/AceLandSettingsProvider.cs
@@ -0,0 +1,51 @@
+﻿using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace AceLand.Library.Editor.Providers
+{
+    public abstract class AceLandSettingsProvider : SettingsProvider
+    {
+        protected internal AceLandSettingsProvider(string path, SettingsScope scope = SettingsScope.User) 
+            : base(path, scope) { }
+        
+        protected SerializedObject Settings;
+        
+        public override void OnActivate(string searchContext, VisualElement rootElement)
+        {
+            base.OnActivate(searchContext, rootElement);
+            Undo.undoRedoPerformed += OnUndoRedoPerformed;
+        }
+
+        public override void OnDeactivate()
+        {
+            base.OnDeactivate();
+            Undo.undoRedoPerformed -= OnUndoRedoPerformed;
+        }
+
+        public override void OnGUI(string searchContext)
+        {
+            EditorGUI.BeginChangeCheck();
+            
+            EditorHelper.DrawAllProperties(Settings);
+            EditorGUILayout.Space(20f);
+            
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(Settings.targetObject, "Apply Changes");
+                Settings.ApplyModifiedProperties();
+            }
+            else
+            {
+                Settings.ApplyModifiedPropertiesWithoutUndo();
+            }
+        }
+        
+        private void OnUndoRedoPerformed()
+        {
+            // Refresh the serialized object when undo/redo is performed
+            Settings.Update();
+            // Optionally, you can also force a repaint if needed
+            EditorUtility.SetDirty(Settings.targetObject);
+        }
+    }
+}
