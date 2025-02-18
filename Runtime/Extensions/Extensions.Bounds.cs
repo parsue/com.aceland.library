069a0a4890b44c991af50e389655202155e3ebdb
Parsue Choi <parsue@gmail.com>
2025-02-18 16:31:45 +0800

    Update Extensions.Bounds.cs

--- Runtime/Extensions/Extensions.Bounds.cs
+++ Runtime/Extensions/Extensions.Bounds.cs
@@ -18,5 +18,18 @@ namespace AceLand.Library.Extensions
             }
             return point;
         }
+
+        public static Bounds WorldBounds(this RectTransform rectTransform)
+        {
+            var corners = new Vector3[4];
+            rectTransform.GetWorldCorners(corners);
+            
+            var bounds = new Bounds(corners[0], Vector3.zero);
+            for (var i = 1; i < corners.Length; i++)
+                bounds.Encapsulate(corners[i]);
+
+            bounds.center = rectTransform.position;
+            return bounds;
+        }
     }
 }
