c0234095b0bec088fb471ae475ec8b1cdab0686c
Parsue Choi <parsue@gmail.com>
2025-02-21 12:46:05 +0800

    Update Extensions.Array.cs

--- Runtime/Extensions/Extensions.Array.cs
+++ Runtime/Extensions/Extensions.Array.cs
@@ -6,13 +6,11 @@ namespace AceLand.Library.Extensions
     {
         public static void SwapElements<T>(this T[] array, int index1, int index2)
         {
-            var lastIndex = array.Length - 1;
-            if (index1 < 0 || index1 > lastIndex)
-                index1 = Mathf.Clamp(index1, 0, array.Length - 1);
-            if (index2 < 0 || index2 > lastIndex)
-                index2 = Mathf.Clamp(index2, 0, array.Length - 1);
-            
+            if (array == null) throw new ArgumentNullException(nameof(array), "Array cannot be null.");
             if (index1 == index2) return;
+            if (index1 < 0 || index1 >= array.Length || index2 < 0 || index2 >= array.Length)
+                throw new ArgumentOutOfRangeException("Indices must be within the bounds of the array.");
+    
             (array[index1], array[index2]) = (array[index2], array[index1]);
         }
     }
