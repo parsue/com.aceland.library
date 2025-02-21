2d56b8f9b9fbc89ac2b587963963da2affd74c09
Parsue Choi <parsue@gmail.com>
2025-02-21 12:46:51 +0800

    Update Extensions.cs

--- Runtime/Extensions/Extensions.cs
+++ Runtime/Extensions/Extensions.cs
@@ -1,9 +1,11 @@
 using Random = System.Random;
+using UnityEngine;
 
 namespace AceLand.Library.Extensions
 {
     public static partial class Extensions
     {
         private static readonly Random Rnd = new();
+        private static readonly Vector3 DesatuationValue = new(0.2126f, 0.7152f, 0.0722f);
     }
 }
