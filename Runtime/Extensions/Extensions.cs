32316de642f0974c9dab17a589fae0e28e513add
Parsue Choi <parsue@gmail.com>
2025-02-21 12:27:22 +0800

    Create Extensions.cs

---
+++ Runtime/Extensions/Extensions.cs
@@ -0,0 +1,9 @@
+using Random = System.Random;
+
+namespace AceLand.Library.Extensions
+{
+    public static partial class Extensions
+    {
+        private static readonly Random Rnd = new();
+    }
+}
