b38256c9e0c18c9d28547984c36416b3a1defd93
Parsue Choi <parsue@gmail.com>
2025-02-21 12:22:49 +0800

    Update Extensions.Bounds.cs

--- Runtime/Extensions/Extensions.Bounds.cs
+++ Runtime/Extensions/Extensions.Bounds.cs
@@ -7,18 +7,6 @@ namespace AceLand.Library.Extensions
     {
         private static readonly Random Rnd = new();
 
-        public static Bounds GetBounds(this RectTransform rectTransform)
-        {
-            var corners = new Vector3[4];
-            rectTransform.GetWorldCorners(corners);
-            
-            var bounds = new Bounds(corners[0], Vector3.zero);
-            for (var i = 1; i < corners.Length; i++)
-                bounds.Encapsulate(corners[i]);
-
-            return bounds;
-        }
-
         public static Vector3 RandomPoint(this Bounds bounds)
         {
             Vector3 point = new(bounds.min.x - 10, bounds.min.y - 10, bounds.min.z -10);
@@ -29,19 +17,6 @@ namespace AceLand.Library.Extensions
                 point.z = ((float)Rnd.NextDouble()).Remap(0f, 1f, bounds.min.z, bounds.max.z);
             }
             return point;
-        }
-
-        public static Bounds WorldBounds(this RectTransform rectTransform)
-        {
-            var corners = new Vector3[4];
-            rectTransform.GetWorldCorners(corners);
-            
-            var bounds = new Bounds(corners[0], Vector3.zero);
-            for (var i = 1; i < corners.Length; i++)
-                bounds.Encapsulate(corners[i]);
-
-            bounds.center = rectTransform.position;
-            return bounds;
         }
     }
 }
