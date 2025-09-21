13cc09a1f107eb66c2ffa0e811d2b8a6e3b89bde
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2025-09-21 10:36:23 +0800

    Lib Project Settings - System Root - Temp Root

---
+++ Runtime/ALib.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using AceLand.Library.ProjectSetting;
+using UnityEngine;
+
+namespace AceLand.Library
+{
+    public static class ALib
+    {
+        internal static AceLandProjectSettings ProjectSettings
+        {
+            get
+            {
+                _projectSettings ??= Resources.Load<AceLandProjectSettings>(nameof(AceLandProjectSettings));
+                return _projectSettings;
+            }
+        }
+        private static AceLandProjectSettings _projectSettings;
+        
+        public static string SystemRootPath => ProjectSettings.SystemRootPath;
+        public static string TempRootPath => ProjectSettings.TempRootPath;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        private static void Initialize()
+        {
+            Directory.CreateDirectory(SystemRootPath);
+            Directory.CreateDirectory(TempRootPath);
+        }
+    }
+}
