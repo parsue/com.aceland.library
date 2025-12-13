218ba179cb5443af1c9d6a322d1b1951fd835726
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2025-12-13 12:10:59 +0800

    handle obsolated methods

--- Editor/AudioHandlers/AudioPreviewer.cs
+++ Editor/AudioHandlers/AudioPreviewer.cs
@@ -9,60 +9,67 @@ namespace AceLand.Library.Editor.AudioHandlers
 {
     public static class AudioPreviewer
     {
-        private static int? ms_lastPlayedAudioClipId = null;
+        private static int? _lastPlayedAudioClipId = null;
 
         [OnOpenAsset]
         public static bool OnOpenAsset(int instanceId, int line)
         {
-            Object obj = EditorUtility.InstanceIDToObject(instanceId);
+#if UNITY_6000_3_OR_NEWER
+            var obj = EditorUtility.EntityIdToObject(instanceId);
+#else
+            var obj = EditorUtility.InstanceIDToObject(instanceId);
+#endif
             if (obj is not AudioClip audioClip) return false;
             if (IsPreviewClipPlaying())
             {
                 StopAllPreviewClips();
-                if (ms_lastPlayedAudioClipId.HasValue && ms_lastPlayedAudioClipId.Value != audioClip.GetInstanceID())
+                if (_lastPlayedAudioClipId.HasValue && _lastPlayedAudioClipId.Value != audioClip.GetInstanceID())
                     PlayPreviewClip(audioClip);
             }
             else
             {
                 PlayPreviewClip(audioClip);
             }
-            ms_lastPlayedAudioClipId = audioClip.GetInstanceID();
+            _lastPlayedAudioClipId = audioClip.GetInstanceID();
             return true;
         }
 
         public static void PlayPreviewClip(AudioClip audioClip)
         {
-            Assembly unityAssembly = typeof(AudioImporter).Assembly;
-            Type audioUtil = unityAssembly.GetType("UnityEditor.AudioUtil");
-            MethodInfo methodInfo = audioUtil.GetMethod(
+            var unityAssembly = typeof(AudioImporter).Assembly;
+            var audioUtil = unityAssembly.GetType("UnityEditor.AudioUtil");
+            var methodInfo = audioUtil.GetMethod(
                 "PlayPreviewClip", 
                 BindingFlags.Static | BindingFlags.Public, 
                 null, 
                 new Type[] {typeof(AudioClip), typeof(Int32), typeof(Boolean)},
                 null
             );
+            if (methodInfo == null) return;
             methodInfo.Invoke(null, new object[] { audioClip, 0, false });
         }
 
         public static bool IsPreviewClipPlaying()
         {
-            Assembly unityAssembly = typeof(AudioImporter).Assembly;
-            Type audioUtil = unityAssembly.GetType("UnityEditor.AudioUtil");
-            MethodInfo methodInfo = audioUtil.GetMethod(
+            var unityAssembly = typeof(AudioImporter).Assembly;
+            var audioUtil = unityAssembly.GetType("UnityEditor.AudioUtil");
+            var methodInfo = audioUtil.GetMethod(
                 "IsPreviewClipPlaying",
                 BindingFlags.Static | BindingFlags.Public
             );
+            if (methodInfo == null) return false;
             return (bool)methodInfo.Invoke(null, null);
         }
 
         public static void StopAllPreviewClips()
         {
-            Assembly unityAssembly = typeof(AudioImporter).Assembly;
-            Type audioUtil = unityAssembly.GetType("UnityEditor.AudioUtil");
-            MethodInfo methodInfo = audioUtil.GetMethod(
+            var unityAssembly = typeof(AudioImporter).Assembly;
+            var audioUtil = unityAssembly.GetType("UnityEditor.AudioUtil");
+            var methodInfo = audioUtil.GetMethod(
                 "StopAllPreviewClips",
                 BindingFlags.Static | BindingFlags.Public
             );
+            if (methodInfo == null) return;
             methodInfo.Invoke(null, null);
         }
     }
--- Editor/HierarchyHandlers/HierarchyScriptDropHandler.cs
+++ Editor/HierarchyHandlers/HierarchyScriptDropHandler.cs
@@ -9,19 +9,35 @@ namespace AceLand.Library.Editor.HierarchyHandlers
     {
         static HierarchyScriptDropHandler()
         {
+#if UNITY_6000_3_OR_NEWER
+            DragAndDrop.AddDropHandlerV2(OnScriptHierarchyDrop);
+#else
             DragAndDrop.AddDropHandler(OnScriptHierarchyDrop);
+#endif
         }
 
-        private static DragAndDropVisualMode OnScriptHierarchyDrop(int dragInstanceId, HierarchyDropFlags dropMode,
-            Transform parentForDraggedObjects, bool perform)
+#if UNITY_6000_3_OR_NEWER
+        private static DragAndDropVisualMode OnScriptHierarchyDrop(
+            EntityId dropTargetEntityId,
+            HierarchyDropFlags dropMode,
+            Transform parentForDraggedObjects,
+            bool perform
+        )
+#else
+        private static DragAndDropVisualMode OnScriptHierarchyDrop(
+            int dragInstanceId,
+            HierarchyDropFlags dropMode,
+            Transform parentForDraggedObjects,
+            bool perform
+        )
+#endif
         {
             var monoScript = GetScriptBeingDragged();
             if (monoScript == null) return DragAndDropVisualMode.None;
-            if (perform)
-            {
-                var gameObject = EditorHelper.CreateAndRename(monoScript.name);
-                gameObject.AddComponent(monoScript.GetClass());
-            }
+            if (!perform) return DragAndDropVisualMode.Copy;
+            
+            var gameObject = EditorHelper.CreateAndRename(monoScript.name);
+            gameObject.AddComponent(monoScript.GetClass());
             return DragAndDropVisualMode.Copy;
         }
 
